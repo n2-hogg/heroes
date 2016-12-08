a4c6ac084da40ffb1e92961170cb88c862fcdedf
Nicola Hogg <nicola2.hogg@live.uwe.ac.uk>
2016-12-08 12:26:13 +0000

    update

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,31 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+
+
+import { routing } from './app.routing';
+import { HeroService } from './hero.service';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    routing,
+    HttpModule
+  ],
+  declarations: [ 
+    AppComponent,
+    HeroDetailComponent, 
+    HeroesComponent,
+    DashboardComponent
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
